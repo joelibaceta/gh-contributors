3862a34bc9df8e1ec80db1ab79c9cedb0631e10f
Joel Ibaceta <mail@joelibaceta.com>
2020-01-10 17:53:34 -0500

    api

--- api/index.py
+++ api/index.py
@@ -19,26 +19,29 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         org = self.get_param('org', self.path)
         repo = self.get_param('repo', self.path)
-        print(org)
-        print(repo)
+        size = self.get_param('size', self.path) 
+        if size is None:
+            size = 32
         output = BytesIO()
-        image = self.getContributors(org, repo)
-        image.save(output)
+        image = self.getContributors(org, repo, size)
+
+        image.save(output, format='PNG')
 
         self.send_response(200)
         self.send_header("Accept-Ranges", "bytes")
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Content-Disposition", "attachment")
-        self.send_header("Content-type", "image/jpeg")
+        self.send_header("Content-type", "image/png")
         self.end_headers()
-        self.wfile.write(output)
+        self.wfile.write(output.getvalue())
         return  
 
-    def getContributors(self, org, repo):
+    def getContributors(self, org, repo, size):
 
         response = requests.get(f'https://api.github.com/repos/{org}/{repo}/contributors')
         contributors = response.json()
         images = []
+        dimension = int(size);
 
         for contributor in contributors:
             avatar_url = contributor["avatar_url"]
@@ -47,12 +50,12 @@ class handler(BaseHTTPRequestHandler):
 
         rows = math.ceil(len(images) / 6)
         spacing = 2 * len(images) - 4
-        new_im = Image.new('RGB', (6*64 + spacing, rows*64  + spacing-8), (255, 255, 255))
+        new_im = Image.new('RGB', (6*dimension + spacing, rows*dimension  + spacing-8), (255, 255, 255))
         x_offset, y_offset = 0, 0
         counter = 1
 
         for im in images:
-            im2 = im.resize((64, 64))
+            im2 = im.resize((dimension, dimension))
             new_im.paste(im2, (x_offset, y_offset))
             if (counter % 6 == 0):
                 y_offset += 2 + im2.size[0]
